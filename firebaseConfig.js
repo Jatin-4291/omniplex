029bfd2f15276c83838ef05cf7ca86e12fb5ae10
Jatin-4291 <jatin.dua2003@gmail.com>
2025-07-21 00:45:57 +0530

    firebase configs

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -3,14 +3,14 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 // Firebase Config
-export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: "",
+const firebaseConfig = {
+  apiKey: "AIzaSyBOXA8suOzOFhOrGMtfb6lZ5wajh4Mu014",
+  authDomain: "omniplex-7e2b7.firebaseapp.com",
+  projectId: "omniplex-7e2b7",
+  storageBucket: "omniplex-7e2b7.firebasestorage.app",
+  messagingSenderId: "724645984265",
+  appId: "1:724645984265:web:6ed5715bb45819dc9f20d8",
+  measurementId: "G-WTHYBGHSSW",
 };
 
 const app = initializeApp(firebaseConfig);
